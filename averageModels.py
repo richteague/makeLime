85dc954684c698742388200cbcfdbc971f7ed372
Rich Teague <teague@mpia.de>
2016-05-23 12:00:00 +0200

    Update averageModels.py

--- averageModels.py
+++ averageModels.py
@@ -5,37 +5,43 @@ from astropy.io import fits
 # Average all the models from LIME. 
 # If asked to, create a .fits file of the standard deviation
 # of each pixel so that one could understand MCMC noise from
-# the gridding.
+# the gridding. If there's only one model, just rename it.
 
 averageModels(nmodels, thetas, phis, transitions,
               returnnoise=True, directory='../'):
 
-    for t in thetas:
-        for p in phis:
-            for j in transitions:
-            
-                # Calculate the averages and move the file to the folder above.
+    if nmodels > 1:
+        for t in thetas:
+            for p in phis:
+                for j in transitions:
                 
-                toaverage = np.array([fits.getdata('%d_%.3f_%.3f_%d.fits' % (m, t, p, j), 0)])
-                averaged = np.average(toaverage, axis=0)
-                hdulist = fits.open('0_%.3f_%.3f_%d.fits' % (t, p, j))
-                hdulist[0].data = averaged
-                hdulist.writeto(fileout+'_%.3f_%.3f_%d.fits' % (t, p, j))
-                os.system('mv %s_%.3f_%.3f_%d.fits %s' % (fileout, i, t, j, directory))
-                
-                # If appropriate, calculate the grid noise.
-                
-                if returnnoise:
-                    gridnoise = np.std(toaverage, axis=0)
+                    # Calculate the averages and move the file to the folder above.
+                    
+                    toaverage = np.array([fits.getdata('%d_%.3f_%.3f_%d.fits' % (m, t, p, j), 0)])
+                    averaged = np.average(toaverage, axis=0)
                     hdulist = fits.open('0_%.3f_%.3f_%d.fits' % (t, p, j))
-                    hdulist[0].data = gridnoise
-                    hdulist.writeto(fileout+'_%.3f_%.3f_%d_noise.fits' % (t, p, j))
-                    os.system('mv %s_%.3f_%.3f_%d_noise.fits %s' % (fileout, t, p, j, directory))
+                    hdulist[0].data = averaged
+                    hdulist.writeto(fileout+'_%.3f_%.3f_%d.fits' % (t, p, j))
+                    os.system('mv %s_%.3f_%.3f_%d.fits %s' % (fileout, i, t, j, directory))
                     
-                    print 'For inclination: %.2f,' % t
-                    print 'position angle: %.2f,' % p
-                    print 'and transition: %d,' % j
-                    print 'we have a grid noise of: %.2f [units].' % (gridnoise.mean())
+                    # If appropriate, calculate the grid noise.
                     
+                    if returnnoise:
+                        gridnoise = np.std(toaverage, axis=0)
+                        hdulist = fits.open('0_%.3f_%.3f_%d.fits' % (t, p, j))
+                        hdulist[0].data = gridnoise
+                        hdulist.writeto(fileout+'_%.3f_%.3f_%d_noise.fits' % (t, p, j))
+                        os.system('mv %s_%.3f_%.3f_%d_noise.fits %s' % (fileout, t, p, j, directory))
+                        
+                        print 'For inclination: %.2f,' % t
+                        print 'position angle: %.2f,' % p
+                        print 'and transition: %d,' % j
+                        print 'we have a grid noise of: %.2f [units].' % (gridnoise.mean())
+        
+        else:
+            for t in thetas:
+                for p in phis:
+                    for j in transitions:
+                        os.system('mv 0_%.3f_%.3f_%d.fits %s' % (t, p, j, directory))
     return
 
