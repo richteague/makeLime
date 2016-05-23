469c3f5f0f932e4b42269d81c55451406694a441
Rich Teague <teague@mpia.de>
2016-05-23 11:26:44 +0200

    Update writeLIME.py

--- writeLIME.py
+++ writeLIME.py
@@ -106,6 +106,9 @@ def inputparameters(pIntensity, sinkPoints, dustfile, molfile, antialias,
         toinsert += line + '\n'
     
     return toinsert
+    
+  
+    
 
 # Write a model.c file for LIME. 
 def generateModelFile(chemheader, model, transitions, stellarmass,
@@ -114,48 +117,21 @@ def generateModelFile(chemheader, model, transitions, stellarmass,
                       velres, pxls, imgres, thetas, phi, distance, unit,
                       collisionfile, orthoratio=None,
                       modelfile='model_template.c', equaltemp=True):
-                      
+
     # Read in the template with which to generate the model.c file.
     # Make sure this template is in your directory.
+    
     with open(modelfile) as f:
         template = f.readlines()
     
-    # Determin the inner and outer radii and the number of cells.
-    rin, rout, ncells = valsfromheader(chemheader)
-
-    # If appropriate, include the difference in dust and gas temperatures.
-    if 'dtemp' in arrsfromheader(chemheader) and not equaltemp:
-        print 'Assuming different dust and gas temperatures.'
-        
-        dstring = ('''temperature[1]=findvalue(hypot(x,y)/AU,fabs(z)/AU,dtemp);\n
-                   if(temperature[1]<2.7){\n
-                   temperature[1] = 2.7;\n
-                   }\n''') 
-     
-        for l, line in enumerate(template):
-            parsedline = ''.join(line.split())
-            if parsedline.startswith('//DustTemperature'):
-                template.insert(l+1, dstring)
-                break
-
-    else:
-        print 'Assuming equal dust and gas temperatures.'
-
-    # Include the gas to dust mass ratio.
-    for l, line in enumerate(template):
-        parsedline = ''.join(line.split())
-        if parsedline.startswith('//GasToDust'):
-            if 'g2d' in arrsfromheader(chemheader):
-                gtd = ('''*gtd=findvalue(hypot(x,y)/AU,fabs(z)/AU,g2d);\n
-                         if(*gtd<50.){\n
-                         *gtd = 50.;\n
-                         }\n''')
-            else:
-                gtd = ('''*gtd = 100.;\n''')
-            template.insert(l+1, gtd)    
-            break
     
-    # Write the stellar mass for the Keplerian rotation.
+    # Include the header file.
+    # Make sure the extension is not included.
+    # Also include the number of chemical model points and inner
+    # and outer radii used in the interpolation routine.
+
+    rin, rout, ncells = valsfromheader(chemheader)
+        
     if chemheader[-2:] == '.h':
         chemheader = chemheader[:-2]
     for l, line in enumerate(template):
@@ -163,76 +139,17 @@ def generateModelFile(chemheader, model, transitions, stellarmass,
         if parsedline.startswith('//ChemicalModel'):
             template.insert(l+1, '#include "%s.h"' % chemheader)
             break    
-        
-    # Write the stellar mass for the Keplerian rotation.
-    for l, line in enumerate(template):
-        parsedline = ''.join(line.split())
-        if parsedline.startswith('//StellarMass'):
-            template.insert(l+1, '#define MSTAR %.2f' % stellarmass)
-            break    
-
-    # Include the number of grid cells for the findcell() function.
+            
     for l, line in enumerate(template):
         parsedline = ''.join(line.split())
         if parsedline.startswith('//GridCells'):
             template.insert(l+1, '#define NCELLS %.d\n' % ncells)
             break
-            
-    # Define the mach number for the turbulent broadening.
-    for l, line in enumerate(template):
-        parsedline = ''.join(line.split())
-        if parsedline.startswith('//Mach'):
-            template.insert(l+1, '#define MACH %.3f\n' % mach)
-            break
     
-    # Assume no o:p ratio and a single H2 density.
-    if orthoratio is None:
-        print 'Assuming {dens} is total H2 density.'
-            
-    # Assume a homogenous o:p ratio and a single total H2 density.                  
-    elif type(orthoratio) is float:
-        print ('''Assuming {dens} is total H2 density
-                  with a %.2f:1.00 o/p ratio.''' 
-                  % (orthoratio/(1.-orthoratio)))
-         
-        pH2string = ('''density[1] = (1. - ORTHORATIO) * density[0];\n
-                       density[0] *= ORTHORATIO;\n''')
-
-        for l, line in enumerate(template):
-            parsedline = ''.join(line.split())
-            if parsedline.startswith('//OrthoRatio'):
-                template.insert(l+1, '#define ORTHORATIO %.3f\n' % orthoratio)
-                break
-
-        for l, line in enumerate(template):
-            parsedline = ''.join(line.split())
-            if parsedline.startswith('//ParaH2Density'):
-                template.insert(l+1, pH2string)
-                break
-                
-        for l, line in enumerate(template):
-            parsedline = ''.join(line.split())
-            if parsedline.startswith('//OrthoCorrection'):
-                template.insert(l+1, 'abundance[0] /= ORTHORATIO;\n')
-                break
-                 
-    # Assume two separate densities, oH2 and pH2.
-    elif type(orthoratio) is str:
-        print ('''Assuming {dens} is oH2 density and {%s} is pH2 density.''' 
-               % orthoratio)
-
-        pH2string = ('''density[1]=findvalue(hypot(x,y)/AU,fabs(z)/AU,%s);\n
-                       if(density[1]<1e-30){\n
-                       density[1] = 1e-30;\n
-                       }\n''' % orthoratio)
-                       
-        for l, line in enumerate(template):
-            parsedline = ''.join(line.split())
-            if parsedline.startswith('//ParaH2Density'):
-                template.insert(l+1, pH2string)
-                break
-
-    # Radiative transfer properties.
+    
+    # LIME model properties.
+    # The inner and outer radii are read from the header file.
+    
     for l, line in enumerate(template):
         parsedline = ''.join(line.split())
         if parsedline.startswith('//InputParameters'):
@@ -246,6 +163,129 @@ def generateModelFile(chemheader, model, transitions, stellarmass,
                                                  rin,
                                                  rout))
             break
+
+
+    # Collider Densities. 
+    # We define orthoratio = n(oH2) / n(pH2). 
+    # orthoratio == None;
+    #   Assume {density} is the total H2 density and let density[0]
+    #   be the total H2 density.
+    # orthoratio == float
+    #   Assume {density} is the total H2 density and scale
+    #   density[0] and density[1] to ortho and para H2 respectively.
+    # orthoratio == string
+    #   Assume {density} is the oH2 density and {string} is the 
+    #   pH2 density, setting density[0] and density[1] respectively.
+    # For orthoratio != None, we need to also rescale the molecular 
+    # abundance as that is given as n(mol) = x(mol) * density[0].
+    
+    if orthoratio is None:
+        print 'Assuming {dens} is total H2 density.'
+                   
+    elif type(orthoratio) is float:
+        print ('''Assuming {dens} is total H2 density
+                  with a %.2f:1.00 o/p ratio.''' 
+                  % (orthoratio/(1.-orthoratio)))
+        pH2string = ('''density[1] = (1. - ORTHORATIO) * density[0];\n
+                       density[0] *= ORTHORATIO;\n''')
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//OrthoRatio'):
+                template.insert(l+1, '#define ORTHORATIO %.3f\n' % orthoratio)
+                break
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//ParaH2Density'):
+                template.insert(l+1, pH2string)
+                break
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//OrthoCorrection'):
+                template.insert(l+1, 'abundance[0] /= ORTHORATIO;\n')
+                break
+                 
+    elif type(orthoratio) is str:
+        print ('''Assuming {dens} is oH2 density and {%s} is pH2 density.''' 
+               % orthoratio)
+        pH2string = ('''density[1]=findvalue(hypot(x,y)/AU,fabs(z)/AU,%s);\n
+                       if(density[1]<1e-30){\n
+                       density[1] = 1e-30;\n
+                       }\n''' % orthoratio)
+        opCstring = ('''(findvalue(hypot(x,y)/AU,fabs(z)/AU,dens) +
+                     findvalue(hypot(x,y)/AU,fabs(z)/AU,%s)) / 
+                     findvalue(hypot(x,y)/AU,fabs(z)/AU,dens) *
+                     abundance[0];\n''' % orthoratio)
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//ParaH2Density'):
+                template.insert(l+1, pH2string)
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//OrthoCorrection'):
+                template.insert(l+1, opCstring)
+                break
+    
+
+    # Dust temperature.
+    # If the header file contains {dtemp} and equaltemp is True, 
+    # we include the dust temperature as temperature[1].
+    
+    if 'dtemp' in arrsfromheader(chemheader) and not equaltemp:
+        print 'Assuming different dust and gas temperatures.'   
+        dstring = ('''temperature[1]=findvalue(hypot(x,y)/AU,fabs(z)/AU,dtemp);\n
+                   if(temperature[1]<2.7){\n
+                   temperature[1]=2.7;\n
+                   }\n''') 
+        for l, line in enumerate(template):
+            parsedline = ''.join(line.split())
+            if parsedline.startswith('//DustTemperature'):
+                template.insert(l+1, dstring)
+                break
+                
+    else:
+        print 'Assuming equal dust and gas temperatures.    
+
+
+    # Gas to dust ratio.
+    # If the header file contains {gtd}, use that with a minimum
+    # value of 50. Otherwise assume standard the 100. 
+    
+    for l, line in enumerate(template):
+        parsedline = ''.join(line.split())
+        if parsedline.startswith('//GasToDust'):
+            if 'g2d' in arrsfromheader(chemheader):
+                gtd = ('''*gtd=findvalue(hypot(x,y)/AU,fabs(z)/AU,g2d);\n
+                         if(*gtd<50.){\n
+                         *gtd = 50.;\n
+                         }\n''')
+            else:
+                gtd = ('''*gtd = 100.;\n''')
+            template.insert(l+1, gtd)    
+            break
+    
+    
+    # Velocity profile, assuming standard Keplerian rotation
+    # Write the stellar mass for the Keplerian rotation.
+
+    for l, line in enumerate(template):
+        parsedline = ''.join(line.split())
+        if parsedline.startswith('//StellarMass'):
+            template.insert(l+1, '#define MSTAR %.2f' % stellarmass)
+            break    
+
+
+    # Define the mach number for the turbulent broadening.
+    # To include only thermal broadening, set mach = 0. 
+    
+    for l, line in enumerate(template):
+        parsedline = ''.join(line.split())
+        if parsedline.startswith('//Mach'):
+            template.insert(l+1, '#define MACH %.3f\n' % mach)
+            break
+    
+   
+    # RUN THE MODELS HERE.
+
     
     # For each transition and inclination specified, add in an image block.
     for i, theta in enumerate(thetas):
