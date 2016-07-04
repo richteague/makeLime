04bbaa0cfee0930b4b230437e829bd9a16906932
Rich Teague <teague@mpia.de>
2016-07-04 10:18:35 +0200

    gas to dust

--- physicalstructure.py
+++ physicalstructure.py
@@ -1,14 +1,12 @@
-# Write the physical structure.
+''' Write the physical structure functions. '''
 
-'''
-    Write the collider density. Three options for opratio:
-    opratio = None  : assumes 'dens' is n(H2) array.
-    opratio = float : assumes an opratio = n(oH2)/n(pH2).
-    opratio = str   : assumes 'dens' is n(oH2) array and opratio is n(pH2) array.
-'''
 def writeDensity(temp, opratio=None): 
+
+    # Collider density.
     temp.append('void density(double x, double y, double z, *double density){\n')
     temp.append('density[0] = findvalue(cone, ctwo, cthree, dens);\n')
+
+    # Ortho/para ratio correction.
     if type(opratio) is float:
         temp.append('density[0] *= (opratio / (1. + opratio));\n') 
         temp.append('density[1] = findvalue(cone, ctwo, cthree, dens);\n')
@@ -24,18 +22,16 @@ def writeDensity(temp, opratio=None):
     return 
 
 
-'''
-    Write the temperature profiles. Three options for dtemp:
-    dtemp = None    : assumes equal gas and dust temperatures.
-    dtemp = float   : assumes dust temp is a homogeneous rescaling, 'dtemp', of the gas temp.
-    dtemp = str     : assumes dust temp given in 'dtemp' array.
-'''
 def writeTemperatures(temp, dtemp=None):
+
+    # Gas temperature.
     temp.append('void temperature(double x, double y, double z, double *temperature){\n')
     temp.append('temperature[0] = findvalue(cone, ctwo, cthree, temp);\n')
     temp.append('if (temperature[0] < 2.73){temperature[0] = 2.73;}\n')
+
+    # Dust temperature.
     if type(dtemp) is float:
-        temp.append('temperature[1] = %.3f * temperature[0];\n' % dtemp)
+        temp.append('temperature[1] = %.3e * temperature[0];\n' % dtemp)
         temp.append('if (temperature[1] < 2.73){temperature[1] = 2.73;}\n')
     elif type(dtemp) is str:
         temp.append('temperature[1] = findvalue(cone, ctwo, cthree, %s);\n' % dtemp)
@@ -43,23 +39,47 @@ def writeTemperatures(temp, dtemp=None):
     elif dtemp is not None:
         raise TypeError("dtemp must be either None, a float or a string.")
     temp.append('}\n')
+
     return
 
 
 def writeAbundance(temp, xmol=None, opratio=None):
-    temp.append('void abundance(double x, double y, double z, double *density){\n')
+
+    # Molecular abundance.
+    temp.append('void abundance(double x, double y, double z, double *abundance){\n')
     if xmol is None:
         temp.append('abundance[0] = findvalue(cone, ctwo, cthree, abund);\n')
     else:
-        temp.append('abundance[0] = %.3f;\n' % float(xmol))
+        temp.append('abundance[0] = %.3e;\n' % float(xmol))
+
+    # Ortho/para correction.
     if type(opratio) is float:
         temp.append('abundance[0] *= (1. + opratio) / opratio;\n')
     elif type(opratio) is str:
         temp.append('double val[2];\n')
         temp.append('density(x, y, z, &val[2]);\n')
         temp.append('abundance[0] *= val[1]/val[0] + 1.;\n')  
-    elif dtemp is not None:
+    elif opratio is not None:
         raise TypeError("opratio must be either None, a float or a string.")
     temp.append('if (abundance[0] < 0.){abundance[0] = 0.;}\n')  
     temp.append('}\n')
+
     return
+
+
+def writeDusttoGas(temp, g2d=None):
+
+    # Gas to dust ratio.
+    if g2d is None:
+        return
+    else 
+        temp.append('void gasIIdust(double x, double y, double z, double *gtd){\n')
+    if type(g2d) is float:
+        temp.append('*gtd = %.3e;\n' % g2d)
+    elif type(g2d) is str:
+        temp.append('*gtd = findvalue(cone, ctwo, cthree, %s);\n' % g2d)
+    temp.append('if *gtd < 1){*gtd = 0.;}\n')
+    temp.append('}\n')
+
+    return
+
