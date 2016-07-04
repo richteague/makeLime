7860e68b9df4523c6e6a06e4faa2fd799b0b4e7c
Rich Teague <teague@mpia.de>
2016-07-04 16:39:28 +0200

    Update physicalstructure.py

--- physicalstructure.py
+++ physicalstructure.py
@@ -2,106 +2,6 @@
 
 import interpolation as interp
 
-# Write the doppler broadening component.
-def writeDopplerBroadening(temp, bvalue=0., btype='absolute', coordsys='cyclindrical', ndim=2):
-
-    #Check that the broadening type is correct.
-    if not (btype is 'absolute' or btype is 'mach'):
-        raise ValueError("btype should be either 'absolute' or 'mach'.")
-    
-    temp.append('void doppler(double x, double y, double z, double *doppler) {\n\n')
-    interp.writeCoords(temp, coordsys, ndim)
-    
-    if btype.lower() == 'absolute':
-        # Constant value.
-        temp.append('\t*doppler = %.1f;\n' % bvalue)
-    else:
-        # Constant mach number.
-        temp.append('\tdouble val[2];\n')
-        temp.append('\ttemperature(x, y, z, &val[2]);\n')
-        temp.append('\t%.2f * sqrt(KBOLTZ * val[0] / 2.34 / AMU);\n')
-        
-    temp.append('\n}\n\n\n')
-
-    return
-
-
-# Write the bulk velocity structure.
-def writeVelocityStructure(temp, stellarmass=None, coordsys='cyclindrical', ndim=2):
-
-    temp.append('void velocity(double x, double y, double z, double *velocity) {\n\n')
-
-    if stellarmass is not None:
-        temp.append('\tvelocity[0] = sqrt(6.67e-11 * %.3f * 2e30 / sqrt(x*x + y*y + z*z));\n' % stellarmass)
-        temp.append('\tvelocity[0] *= sin(atan2(y,x));\n')
-        temp.append('\tvelocity[1] = sqrt(6.67e-11 * %.3f * 2e30 / sqrt(x*x + y*y + z*z));\n' % stellarmass)
-        temp.append('\tvelocity[1] *= cos(atan2(y,x));\n')
-        temp.append('\tvelocity[2] = 0.0;\n')
-
-    else:
-        temp.append('\tvelocity[0] = findvalue(cone, ctwo, cthree, velx);\n')
-        temp.append('\tvelocity[1] = findvalue(cone, ctwo, cthree, vely);\n')
-        temp.append('\tvelocity[2] = findvalue(cone, ctwo, cthree, velz);\n')
-        
-    temp.append('\n}\n\n\n')
-
-    return
-
-
-# Write the main collider density.
-def writeDensity(temp, opratio=None, coordsys='cyclindrical', ndim=2): 
-
-    temp.append('void density(double x, double y, double z, *double density) {\n\n')
-    interp.writeCoords(temp, coordsys, ndim)
-    
-    temp.append('\tdensity[0] = findvalue(cone, ctwo, cthree, dens);\n')
-
-    if type(opratio) is int:
-        opratio = float(opratio)
-
-    if type(opratio) is float:
-        temp.append('\tdensity[0] *= (opratio / (1. + opratio));\n') 
-        temp.append('\tdensity[1] = findvalue(cone, ctwo, cthree, dens);\n')
-        temp.append('\tdensity[1] /= (1. + opratio);\n') 
-        temp.append('\tif (density[1] < 1e-30) {\n\tdensity[1] = 1e-30;\n\t}\n\n')
-
-    elif type(opratio) is str:
-        temp.append('\tdensity[1] = findvalue(cone, ctwo, cthree, %s);\n' % opratio)
-        temp.append('\tif (density[1] < 1e-30) {\n\t\tdensity[1] = 1e-30;\n\t}\n\n')
-
-    elif opratio is not None:
-        raise TypeError("opratio must be either None, a float or a string.")
-
-    temp.append('\tif (density[0] < 1e-30) {\n\t\tdensity[0] = 1e-30;\n\t}\n\n')
-    temp.append('}\n\n\n')
-
-    return 
-
-
-# Write the gas and dust temperatures.
-def writeTemperatures(temp, dtemp=None, coordsys='cyclindrical', ndim=2):
-
-    temp.append('void temperature(double x, double y, double z, double *temperature) {\n\n')
-    interp.writeCoords(temp, coordsys, ndim)
-
-    temp.append('\ttemperature[0] = findvalue(cone, ctwo, cthree, temp);\n')
-    temp.append('\tif (temperature[0] < 2.73) {\n\t\ttemperature[0] = 2.73;\n\t}\n\n')
-
-    if type(dtemp) is float:
-        temp.append('\ttemperature[1] = %.3f * temperature[0];\n' % dtemp)
-        temp.append('\tif (temperature[1] < 2.73){temperature[1] = 2.73;}\n')
-
-    elif type(dtemp) is str:
-        temp.append('\ttemperature[1] = findvalue(cone, ctwo, cthree, %s);\n' % dtemp)
-        temp.append('\tif (temperature[1] < 2.73) {\n\t\ttemperature[1] = 2.73;\n\t}\n')
-
-    elif dtemp is not None:
-        raise TypeError("dtemp must be either None, a float or a string.")
-
-    temp.append('}\n\n\n')
-
-    return
-
 
 # Write the molecular abundances.
 def writeAbundance(temp, xmol=None, opratio=None, coordsys='cyclindrical', ndim=2):
@@ -133,6 +33,59 @@ def writeAbundance(temp, xmol=None, opratio=None, coordsys='cyclindrical', ndim=
 
     return
 
+# Write the main collider density.
+def writeDensity(temp, opratio=None, coordsys='cyclindrical', ndim=2): 
+
+    temp.append('void density(double x, double y, double z, *double density) {\n\n')
+    interp.writeCoords(temp, coordsys, ndim)
+    
+    temp.append('\tdensity[0] = findvalue(cone, ctwo, cthree, dens);\n')
+
+    if type(opratio) is int:
+        opratio = float(opratio)
+
+    if type(opratio) is float:
+        temp.append('\tdensity[0] *= (opratio / (1. + opratio));\n') 
+        temp.append('\tdensity[1] = findvalue(cone, ctwo, cthree, dens);\n')
+        temp.append('\tdensity[1] /= (1. + opratio);\n') 
+        temp.append('\tif (density[1] < 1e-30) {\n\tdensity[1] = 1e-30;\n\t}\n\n')
+
+    elif type(opratio) is str:
+        temp.append('\tdensity[1] = findvalue(cone, ctwo, cthree, %s);\n' % opratio)
+        temp.append('\tif (density[1] < 1e-30) {\n\t\tdensity[1] = 1e-30;\n\t}\n\n')
+
+    elif opratio is not None:
+        raise TypeError("opratio must be either None, a float or a string.")
+
+    temp.append('\tif (density[0] < 1e-30) {\n\t\tdensity[0] = 1e-30;\n\t}\n\n')
+    temp.append('}\n\n\n')
+
+    return 
+
+
+# Write the doppler broadening component.
+def writeDopplerBroadening(temp, bvalue=0., btype='absolute', coordsys='cyclindrical', ndim=2):
+
+    #Check that the broadening type is correct.
+    if not (btype is 'absolute' or btype is 'mach'):
+        raise ValueError("btype should be either 'absolute' or 'mach'.")
+    
+    temp.append('void doppler(double x, double y, double z, double *doppler) {\n\n')
+    interp.writeCoords(temp, coordsys, ndim)
+    
+    if btype.lower() == 'absolute':
+        # Constant value.
+        temp.append('\t*doppler = %.1f;\n' % bvalue)
+    else:
+        # Constant mach number.
+        temp.append('\tdouble val[2];\n')
+        temp.append('\ttemperature(x, y, z, &val[2]);\n')
+        temp.append('\t%.2f * sqrt(KBOLTZ * val[0] / 2.34 / AMU);\n')
+        
+    temp.append('\n}\n\n\n')
+
+    return
+
 
 # Write the gas-to-dust ratio.
 def writeGastoDust(temp, g2d=None, coordsys='cyclindrical', ndim=2):
@@ -154,3 +107,49 @@ def writeGastoDust(temp, g2d=None, coordsys='cyclindrical', ndim=2):
 
     return
 
+
+# Write the gas and dust temperatures.
+def writeTemperatures(temp, dtemp=None, coordsys='cyclindrical', ndim=2):
+
+    temp.append('void temperature(double x, double y, double z, double *temperature) {\n\n')
+    interp.writeCoords(temp, coordsys, ndim)
+
+    temp.append('\ttemperature[0] = findvalue(cone, ctwo, cthree, temp);\n')
+    temp.append('\tif (temperature[0] < 2.73) {\n\t\ttemperature[0] = 2.73;\n\t}\n\n')
+
+    if type(dtemp) is float:
+        temp.append('\ttemperature[1] = %.3f * temperature[0];\n' % dtemp)
+        temp.append('\tif (temperature[1] < 2.73){temperature[1] = 2.73;}\n')
+
+    elif type(dtemp) is str:
+        temp.append('\ttemperature[1] = findvalue(cone, ctwo, cthree, %s);\n' % dtemp)
+        temp.append('\tif (temperature[1] < 2.73) {\n\t\ttemperature[1] = 2.73;\n\t}\n')
+
+    elif dtemp is not None:
+        raise TypeError("dtemp must be either None, a float or a string.")
+
+    temp.append('}\n\n\n')
+
+    return
+
+
+# Write the bulk velocity structure.
+def writeVelocityStructure(temp, stellarmass=None, coordsys='cyclindrical', ndim=2):
+
+    temp.append('void velocity(double x, double y, double z, double *velocity) {\n\n')
+
+    if stellarmass is not None:
+        temp.append('\tvelocity[0] = sqrt(6.67e-11 * %.3f * 2e30 / sqrt(x*x + y*y + z*z));\n' % stellarmass)
+        temp.append('\tvelocity[0] *= sin(atan2(y,x));\n')
+        temp.append('\tvelocity[1] = sqrt(6.67e-11 * %.3f * 2e30 / sqrt(x*x + y*y + z*z));\n' % stellarmass)
+        temp.append('\tvelocity[1] *= cos(atan2(y,x));\n')
+        temp.append('\tvelocity[2] = 0.0;\n')
+
+    else:
+        temp.append('\tvelocity[0] = findvalue(cone, ctwo, cthree, velx);\n')
+        temp.append('\tvelocity[1] = findvalue(cone, ctwo, cthree, vely);\n')
+        temp.append('\tvelocity[2] = findvalue(cone, ctwo, cthree, velz);\n')
+        
+    temp.append('\n}\n\n\n')
+
+    return
