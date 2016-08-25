1c24c26a25f3c1ee9398c6da557e691c5aa5fa76
Rich Teague <teague@mpia.de>
2016-08-25 14:17:25 +0200

    flake8 on runlime.py

--- runlime.py
+++ runlime.py
@@ -1,7 +1,6 @@
 ''' Script to run a batch of Lime jobs. '''
 
 import os
-import sys
 import time
 import makemodelfile as make
 import limeclass as lime
@@ -29,8 +28,8 @@ def seconds2hms(seconds):
 
 # Run LIME models.
 def runLime(name='tempmodelname',
-            headerfile='header.h', 
-            moldatfile='molecularrates.dat', 
+            headerfile='header.h',
+            moldatfile='molecularrates.dat',
             transitions=[0],
             inclinations=[0],
             positionangles=[0],
@@ -40,12 +39,12 @@ def runLime(name='tempmodelname',
             sinkPoints=3e3,
             antialias=1,
             sampling=2,
-            lte_only=1, 
+            lte_only=1,
             imgres=0.065,
-            distance=54., 
-            pxls=128, 
-            units=0, 
-            stellarmass=0.6, 
+            distance=54.,
+            pxls=128,
+            unit=0,
+            stellarmass=0.6,
             outputfile=False,
             binoutputfile=False,
             gridfile=False,
@@ -61,15 +60,13 @@ def runLime(name='tempmodelname',
             directory='../',
             nmodels=1,
             returnnoise=False,
-            cleanup=True, 
+            cleanup=True,
             waittime=20):
 
     # Start the clock to time the running of models.
-    
     t0 = time.time()
 
     # Create the temporary folder to work in and move there.
-    
     fname = makeUniqueFolder()
     os.chdir(fname)
     path = os.path.dirname(__file__) + '/AuxFiles'
@@ -78,11 +75,10 @@ def runLime(name='tempmodelname',
     os.system('cp %s/%s .' % (path, dust))
 
     # Generate a LIME model class instance.
-    
     model = lime.model(name=name,
                        headerfile=headerfile,
                        moldatfile=moldatfile,
-                       transitions=transitions, 
+                       transitions=transitions,
                        inclinations=inclinations,
                        positionangles=positionangles,
                        nchan=nchan,
@@ -105,7 +101,7 @@ def runLime(name='tempmodelname',
                        dtemp=dtemp,
                        abund=abund,
                        g2d=g2d,
-                       doppler=doppler, 
+                       doppler=doppler,
                        dopplertype=dopplertype,
                        coordsys=coordsys,
                        dust=dust,
@@ -114,52 +110,48 @@ def runLime(name='tempmodelname',
                        returnnoise=returnnoise,
                        )
 
-    print '\n'                       
-    print 'Assuming input is %dD-%s coordinates.' % (model.ndim, model.coordsys)
-    print 'Found model bounds of  %.2f au and %.2f au.' % (model.rin, model.rout) 
+    print 'Input is %dD-%s coordinates.' % (model.ndim, model.coordsys)
+    print 'Model bounds are %.2f and %.2f au' % (model.rin, model.rout)
 
     # For each iteration, run a model with a pause of waittime seconds.
-    
     print '\n'
     for m in range(nmodels):
         print 'Running model %d of %d.' % (m+1, nmodels)
         make.makeFile(m, model)
-        os.system('nohup lime -n -f -p 20 model_%d.c >nohup_%d.out 2>&1 &' % (m, m))
+        cmd = 'nohup lime -n -f -p 20 model_%d.c >nohup_%d.out 2>&1 &' % (m, m)
+        os.system(cmd)
         waittime = max(10., waittime)
         time.sleep(waittime)
 
     # Make sure all the models have run.
-    
     remaining = -1
     print '\n'
-    while len([fn for fn in os.listdir(os.curdir) if fn.endswith('.x')]) > 0:
-        newremaining = len([fn for fn in os.listdir(os.curdir) if fn.endswith('.x')])
-        if newremaining != remaining:
-            print 'Waiting on %d models to run.' % newremaining
-            remaining = newremaining
+    while len([fn for fn in os.listdir('./') if fn.endswith('.x')]) > 0:
+        nremaining = len([fn for fn in os.listdir('./') if fn.endswith('.x')])
+        if nremaining != remaining:
+            print 'Waiting on %d models to run.' % nremaining
+            remaining = nremaining
         time.sleep(10*remaining)
     if len([fn for fn in os.listdir('./') if fn.endswith('.fits')]) < nmodels:
-        print 'Not all models were successfully run. Aborting without clean-up.\n'
+        print 'Not all models were successfully run.'
+        print 'Aborting without clean-up.'
         return
     else:
         print 'All instances complete.\n'
 
     # Combine the model ensemble.
-    
     make.averageModels(model)
     make.combinePopfiles(model)
     make.combineBinPopFiles(model)
     make.getNoise(model)
 
     # Clean up.
-    
     os.chdir('../')
     if cleanup:
         print 'Cleaning up temporary folders.'
         os.system('rm -rf %s' % fname)
 
     # Print the total time.
-    
     print 'Finished in %s.\n\n' % seconds2hms(time.time() - t0)
 
     return
